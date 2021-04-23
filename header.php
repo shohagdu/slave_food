c50de364e1fd8a3cf9720661c8d870d31f1072de
omarshohagdu <omarfaruk@du.ac.bd>
2021-04-23 12:21:26 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>খাদ্য বান্ধব কর্মসূচি, Step Technology</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/w3.css">
+<script src = "assets/jquery.min.js"></script>
+<body>
+<div class="w3-top">
+    <div class="w3-bar w3-black w3-card">
+        <a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+        <a href="msAccesstomySQL.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Download Data</a>
+        <a href="sync.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Sync to Live</a>
+        <a href="dataProcess.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Data Process</a>
+    </div>
+</div>
+<div class="w3-content" style="max-width:2000px;">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:1000px;min-height:550px;background: #f1f1f1" id="band" >
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+        include 'header.php';
+        echo "Home content is being updating...";
+        include 'footer.php';
+?>
+
+
+
+
